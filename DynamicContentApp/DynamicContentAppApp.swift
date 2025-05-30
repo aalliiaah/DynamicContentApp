34f07655ed9a05b17b13997d4f83ca1b3b2ad263
aalliiaah <aliah0909abdulrhman@gmail.com>
2025-05-30 20:38:44 +0300

    Initial Commit

---
+++ DynamicContentApp/DynamicContentAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  DynamicContentAppApp.swift
+//  DynamicContentApp
+//
+//  Created by Aliah Alhameed on 03/12/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct DynamicContentAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ DynamicContentAppTests/DynamicContentAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicContentAppTests.swift
+//  DynamicContentAppTests
+//
+//  Created by Aliah Alhameed on 03/12/1446 AH.
+//
+
+import Testing
+@testable import DynamicContentApp
+
+struct DynamicContentAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
